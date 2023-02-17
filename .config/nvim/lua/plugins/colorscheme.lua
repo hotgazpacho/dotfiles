3d8968505884f629511933504f0fad0e7c40db3b
Will Green <will@hotgazpacho.org>
2023-02-16 21:56:51 -0500

    feat(nvim): switch solarized scheme to maxmx03/solarized.nvim

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,21 @@
 return {
-  -- https://github.com/ishan9299/nvim-solarized-lua
-  { "ishan9299/nvim-solarized-lua" },
+  -- https://github.com/maxmx03/solarized.nvim
+  {
+    "maxmx03/solarized.nvim",
+    config = function()
+      local success, solarized = pcall(require, "solarized")
+
+      if not success then
+        return
+      end
+
+      solarized.setup({
+        mode = "dark",
+        theme = "vim",
+        transparnet = true,
+      })
+    end,
+  },
   -- Configure LazyVim to load solarized
   {
     "LazyVim/LazyVim",
