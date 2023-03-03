34e1c67c29462ee1db8c0198408d22760d055a78
Will Green <will@hotgazpacho.org>
2023-03-03 07:59:06 -0500

    fix(nvim): correct golang spec & update deps

--- .config/nvim/lua/plugins/golang.lua
+++ .config/nvim/lua/plugins/golang.lua
@@ -2,7 +2,7 @@ return {
   {
     "ray-x/go.nvim",
     lazy = true,
-    requires = { -- optional packages
+    dependencies = { -- optional packages
       "ray-x/guihua.lua",
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
