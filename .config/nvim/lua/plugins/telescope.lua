8fd8d3722b41c444bbf8f904a34c38d6c897e12d
Will Green <will@hotgazpacho.org>
2023-03-11 22:37:47 -0500

    feat(nvim): add fzf telescope native

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,25 @@
+return {
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>fp",
+        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        desc = "Find Plugin File",
+      },
+    },
+  },
+}
