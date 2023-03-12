9afaa99dc7dda730cbd28360b1e2b7eb12f03808
Will Green <will@hotgazpacho.org>
2023-03-12 12:24:09 -0400

    feat(nvim): add ToggleTerm and flatten

---
+++ .config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "willothy/flatten.nvim",
+    opts = {
+      callbacks = {
+        pre_open = function()
+          require("toggleterm").toggle()
+        end,
+        post_open = function(_, winnr)
+          local term = require("toggleterm.terminal").get_last_focused() -- <--- Here
+          if not term or not term:is_float() then
+            term:toggle()
+          end
+          api.nvim_set_current_win(winnr)
+        end,
+        block_end = function()
+          require("toggleterm").toggle()
+        end,
+      },
+      -- <String, Bool> dictionary of filetypes that should be blocking
+      block_for = {
+        gitcommit = true,
+      },
+      -- Window options
+      window = {
+        -- Options:
+        -- tab            -> open in new tab (default)
+        -- split          -> open in split
+        -- vsplit         -> open in vsplit
+        -- current        -> open in current window
+        -- func(new_bufs) -> only open the files, allowing you to handle window opening yourself.
+        -- Argument is an array of buffer numbers representing the newly opened files.
+        open = "tab",
+        -- Affects which file gets focused when opening multiple at once
+        -- Options:
+        -- "first"        -> open first file of new files (default)
+        -- "last"         -> open last file of new files
+        focus = "first",
+      },
+    },
+  },
+  {
+    "akinsho/toggleterm.nvim",
+    cmd = { "ToggleTerm", "TermExec" },
+    opts = {
+      -- size can be a number or function which is passed the current terminal
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 10
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      open_mapping = [[<C-\>]],
+      shading_factor = 2,
+      direction = "vertical",
+      float_opts = {
+        border = "curved",
+        highlights = { border = "Normal", background = "Normal" },
+      },
+      winbar = {
+        enabled = false,
+        name_formatter = function(term) --  term: Terminal
+          return term.name
+        end,
+      },
+    },
+  },
+}
