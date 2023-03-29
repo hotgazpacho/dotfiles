57c08433d55122ea4efceca77801703c0f7b9906
Will Green <will@hotgazpacho.org>
2023-03-28 22:17:03 -0400

    feat(nvim): add nvim-dap-vscode-js to use the vscode js&ts debugger

--- .config/nvim/lua/plugins/debugger.lua
+++ .config/nvim/lua/plugins/debugger.lua
@@ -6,7 +6,13 @@ return {
         "jay-babu/mason-nvim-dap.nvim",
         dependencies = { "nvim-dap" },
         cmd = { "DapInstall", "DapUninstall" },
-        opts = { automatic_setup = true },
+        opts = {
+          automatic_setup = true,
+          ensure_installed = {
+            "js-debug-adapter",
+            "delve",
+          },
+        },
         config = function(_, opts)
           local mason_nvim_dap = require("mason-nvim-dap")
           mason_nvim_dap.setup(opts)
@@ -42,6 +48,54 @@ return {
       {
         "theHamsta/nvim-dap-virtual-text",
         config = true,
+      },
+      {
+        "mxsdev/nvim-dap-vscode-js",
+        dependencies = { "nvim-dap" },
+        opts = {
+          debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter",
+          debugger_cmd = { "js-debug-adapter" },
+          adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+        },
+        config = function(_, opts)
+          require("dap-vscode-js").setup(opts)
+
+          for _, language in ipairs({ "typescript", "javascript" }) do
+            require("dap").configurations[language] = {
+              {
+                {
+                  type = "pwa-node",
+                  request = "launch",
+                  name = "Launch file",
+                  program = "${file}",
+                  cwd = "${workspaceFolder}",
+                },
+                {
+                  type = "pwa-node",
+                  request = "attach",
+                  name = "Attach",
+                  processId = require("dap.utils").pick_process,
+                  cwd = "${workspaceFolder}",
+                },
+              },
+              {
+                type = "pwa-node",
+                request = "launch",
+                name = "Debug Jest Tests",
+                -- trace = true, -- include debugger info
+                runtimeExecutable = "node",
+                runtimeArgs = {
+                  "./node_modules/jest/bin/jest.js",
+                  "--runInBand",
+                },
+                rootPath = "${workspaceFolder}",
+                cwd = "${workspaceFolder}",
+                console = "integratedTerminal",
+                internalConsoleOptions = "neverOpen",
+              },
+            }
+          end
+        end,
       },
     },
   },
