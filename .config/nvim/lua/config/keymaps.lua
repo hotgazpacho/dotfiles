f3619e1c925d679f09a4cbf6e7e7d11a87e28f29
Will Green <will@hotgazpacho.org>
2023-04-01 09:07:46 -0400

    feat(nvim): add gitlinker and git-related keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -34,3 +34,41 @@ if Util.has("toggleterm.nvim") then
   vim.keymap.set("n", "<leader>t<tab>", "<cmd>ToggleTerm direction=tab<cr>", { desc = "ToggleTerm tab" })
   vim.keymap.set("n", "<leader>tn", "<cmd>ToggleTermSetName<cr>", { desc = "Set Terminal Name" })
 end
+
+-- Git keymaps
+if Util.has("gitlinker.nvim") then
+  vim.keymap.set(
+    "n",
+    "<leader>gy",
+    '<cmd>lua require"gitlinker".get_buf_range_url("n")<cr>',
+    { desc = "Copy git link", silent = true }
+  )
+  vim.keymap.set(
+    "v",
+    "<leader>gy",
+    '<cmd>lua require"gitlinker".get_buf_range_url("v")<cr>',
+    { desc = "Copy git link" }
+  )
+  vim.keymap.set(
+    "n",
+    "<leader>gb",
+    '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+    { desc = "Open git link in browser", silent = true }
+  )
+  vim.keymap.set(
+    "v",
+    "<leader>gb",
+    '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+    { desc = "Open git link in browser" }
+  )
+end
+
+-- Keymaps for Octo
+if Util.has("octo.nvim") then
+  wk.register({
+    ["<leader>gh"] = { name = "+github" },
+    ["<leader>ghp"] = { name = "+pr" },
+  })
+  vim.keymap.set({ "n", "v" }, "<leader>ghs", "<cmd>Octo search<cr>", { desc = "Search Issues and PRs" })
+  vim.keymap.set({ "n", "v" }, "<leader>ghps", "<cmd>Octo pr search<cr>", { desc = "Search Pull Requests" })
+end
--- .config/nvim/lua/plugins/github.lua
+++ .config/nvim/lua/plugins/github.lua
@@ -5,4 +5,17 @@ return {
     cmd = "Octo",
     config = true,
   },
+  {
+    "ruifm/gitlinker.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true,
+    opts = function()
+      local opts = require("gitlinker.opts").get()
+      local hosts = require("gitlinker.hosts")
+      opts.callbacks = hosts.callbacks
+      opts.mappings = nil
+      return opts
+    end,
+    keys = { "<leader>g" },
+  },
 }
