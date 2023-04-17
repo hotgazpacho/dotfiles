02fc6128b25003c490318bc98681e2b4c2d80638
Will Green <will@hotgazpacho.org>
2023-04-16 20:38:49 -0400

    feat(nvim): move debugger config to LazyVim's DAP extra

--- .config/nvim/lua/plugins/debugger.lua
+++ .config/nvim/lua/plugins/debugger.lua
@@ -1,102 +1,60 @@
 return {
   {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      {
-        "jay-babu/mason-nvim-dap.nvim",
-        dependencies = { "nvim-dap" },
-        cmd = { "DapInstall", "DapUninstall" },
-        opts = {
-          automatic_setup = true,
-          ensure_installed = {
-            "js-debug-adapter",
-            "delve",
-          },
-        },
-        config = function(_, opts)
-          local mason_nvim_dap = require("mason-nvim-dap")
-          mason_nvim_dap.setup(opts)
-          mason_nvim_dap.default_setup({})
-        end,
-      },
-      {
-        "rcarriga/nvim-dap-ui",
-        opts = { floating = { border = "rounded" } },
-        config = function()
-          local dap, dapui = require("dap"), require("dapui")
-          dap.listeners.after.event_initialized["dapui_config"] = function()
-            dapui.open()
-          end
-          dap.listeners.before.event_terminated["dapui_config"] = function()
-            dapui.close()
-          end
-          dap.listeners.before.event_exited["dapui_config"] = function()
-            dapui.close()
-          end
-          dapui.setup()
-        end,
-      },
-      {
-        "folke/neodev.nvim",
-        opts = {
-          library = {
-            plugins = { "nvim-dap-ui" },
-            types = true,
-          },
-        },
-      },
-      {
-        "theHamsta/nvim-dap-virtual-text",
-        config = true,
-      },
-      {
-        "mxsdev/nvim-dap-vscode-js",
-        dependencies = { "nvim-dap" },
-        opts = {
-          debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter",
-          debugger_cmd = { "js-debug-adapter" },
-          adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-        },
-        config = function(_, opts)
-          require("dap-vscode-js").setup(opts)
-
-          for _, language in ipairs({ "typescript", "javascript" }) do
-            require("dap").configurations[language] = {
-              {
-                {
-                  type = "pwa-node",
-                  request = "launch",
-                  name = "Launch file",
-                  program = "${file}",
-                  cwd = "${workspaceFolder}",
-                },
-                {
-                  type = "pwa-node",
-                  request = "attach",
-                  name = "Attach",
-                  processId = require("dap.utils").pick_process,
-                  cwd = "${workspaceFolder}",
-                },
-              },
-              {
-                type = "pwa-node",
-                request = "launch",
-                name = "Debug Jest Tests",
-                -- trace = true, -- include debugger info
-                runtimeExecutable = "node",
-                runtimeArgs = {
-                  "./node_modules/jest/bin/jest.js",
-                  "--runInBand",
-                },
-                rootPath = "${workspaceFolder}",
-                cwd = "${workspaceFolder}",
-                console = "integratedTerminal",
-                internalConsoleOptions = "neverOpen",
-              },
-            }
-          end
-        end,
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      automatic_setup = true,
+      ensure_installed = {
+        "js-debug-adapter",
+        "delve",
       },
     },
+  },
+  {
+    "mxsdev/nvim-dap-vscode-js",
+    dependencies = { "nvim-dap" },
+    opts = {
+      debugger_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter",
+      debugger_cmd = { "js-debug-adapter" },
+      adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
+    },
+    config = function(_, opts)
+      require("dap-vscode-js").setup(opts)
+
+      for _, language in ipairs({ "typescript", "javascript" }) do
+        require("dap").configurations[language] = {
+          {
+            {
+              type = "pwa-node",
+              request = "launch",
+              name = "Launch file",
+              program = "${file}",
+              cwd = "${workspaceFolder}",
+            },
+            {
+              type = "pwa-node",
+              request = "attach",
+              name = "Attach",
+              processId = require("dap.utils").pick_process,
+              cwd = "${workspaceFolder}",
+            },
+          },
+          {
+            type = "pwa-node",
+            request = "launch",
+            name = "Debug Jest Tests",
+            -- trace = true, -- include debugger info
+            runtimeExecutable = "node",
+            runtimeArgs = {
+              "./node_modules/jest/bin/jest.js",
+              "--runInBand",
+            },
+            rootPath = "${workspaceFolder}",
+            cwd = "${workspaceFolder}",
+            console = "integratedTerminal",
+            internalConsoleOptions = "neverOpen",
+          },
+        }
+      end
+    end,
   },
 }
