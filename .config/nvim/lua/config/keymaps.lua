625b4bb4286ccd79cf3289bed6a991ec21156388
Will Green <will@hotgazpacho.org>
2023-04-20 23:38:03 -0400

    refactor(nvim): move git-related keymaps near their plugins

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,9 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local Util = require("lazyvim.util")
-local wk = require("which-key")
-
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Return to normal mode" })
 
 vim.keymap.set({ "n", "v" }, "<leader>cg", function()
@@ -11,49 +8,6 @@ vim.keymap.set({ "n", "v" }, "<leader>cg", function()
 end, { desc = "Go to Definition" })
 vim.keymap.set({ "n", "v" }, "<leader>cr", "<cmd>Telescope lsp_references<cr>", { desc = "References" })
 vim.keymap.set({ "n", "v" }, "<leader>ci", "<cmd>Telescope lsp_incoming_calls<cr>", { desc = "Incoming Calls" })
-
--- Git keymaps
-if Util.has("gitlinker.nvim") then
-  vim.keymap.set(
-    "n",
-    "<leader>gy",
-    '<cmd>lua require"gitlinker".get_buf_range_url("n")<cr>',
-    { desc = "Copy git link", silent = true }
-  )
-  vim.keymap.set(
-    "v",
-    "<leader>gy",
-    '<cmd>lua require"gitlinker".get_buf_range_url("v")<cr>',
-    { desc = "Copy git link" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>gb",
-    '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-    { desc = "Open git link in browser", silent = true }
-  )
-  vim.keymap.set(
-    "v",
-    "<leader>gb",
-    '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-    { desc = "Open git link in browser" }
-  )
-end
-
--- Keymaps for Octo
-if Util.has("octo.nvim") then
-  wk.register({
-    ["<leader>gb"] = { name = "+github" },
-    ["<leader>gbp"] = { name = "+pullrequest" },
-  })
-  vim.keymap.set("n", "<leader>gbs", "<cmd>Octo search<cr>", { desc = "Search" })
-  vim.keymap.set("n", "<leader>gbb", "<cmd>Octo repo browser<cr>", { desc = "open in Browser" })
-  vim.keymap.set("n", "<leader>gbpc", "<cmd>Octo pr checks<cr>", { desc = "Checks status" })
-  vim.keymap.set("n", "<leader>gbps", "<cmd>Octo pr search<cr>", { desc = "Search" })
-  vim.keymap.set("n", "<leader>gbpr", "<cmd>Octo pr ready<cr>", { desc = "Mark draft Ready for Review" })
-  vim.keymap.set("n", "<leader>gbpv", "<cmd>Octo pr browser<cr>", { desc = "View in browser" })
-  vim.keymap.set("n", "<leader>gbpu", "<cmd>Octo pr url<cr>", { desc = "copy URL" })
-end
 
 -- neoclip
 vim.keymap.set("n", "<leader>sy", "<cmd>Telescope neoclip unnamed<cr>", { desc = "Clipboard (default register)" })
--- .config/nvim/lua/plugins/github.lua
+++ .config/nvim/lua/plugins/github.lua
@@ -1,14 +1,36 @@
 return {
   {
     "pwntester/octo.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+      {
+        "folke/which-key.nvim",
+        opts = {
+          defaults = {
+            ["<leader>gb"] = { name = "+github" },
+            ["<leader>gp"] = { name = "+pullrequest" },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("octo").setup(opts)
+    end,
     cmd = "Octo",
-    config = true,
+    keys = {
+      { "<leader>gbs", "<cmd>Octo search<cr>", desc = "Search" },
+      { "<leader>gbb", "<cmd>Octo repo browser<cr>", desc = "open in Browser" },
+      { "<leader>gpc", "<cmd>Octo pr checks<cr>", desc = "Checks status" },
+      { "<leader>gps", "<cmd>Octo pr search<cr>", desc = "Search" },
+      { "<leader>gpr", "<cmd>Octo pr ready<cr>", desc = "mark draft Ready" },
+      { "<leader>gpb", "<cmd>Octo pr browser<cr>", desc = "view in Browser" },
+      { "<leader>gpu", "<cmd>Octo pr url<cr>", desc = "copy URL" },
+    },
   },
   {
     "ruifm/gitlinker.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = true,
     opts = function()
       local opts = require("gitlinker.opts").get()
       local hosts = require("gitlinker.hosts")
@@ -16,6 +38,64 @@ return {
       opts.mappings = nil
       return opts
     end,
-    keys = { "<leader>g" },
+    keys = {
+      {
+        "<leader>gy",
+        function()
+          require("gitlinker").get_buf_range_url("n")
+        end,
+        desc = "Copy git link",
+        mode = { "n" },
+      },
+      {
+        "<leader>gy",
+        function()
+          require("gitlinker").get_buf_range_url("v")
+        end,
+        desc = "Copy git link",
+        mode = { "v" },
+      },
+      {
+        "<leader>gY",
+        function()
+          require("gitlinker").get_repo_url()
+        end,
+        desc = "Copy repo url",
+        mode = { "n" },
+      },
+      {
+        "<leader>gb",
+        function()
+          require("gitlinker").get_buf_range_url(
+            "n",
+            { action_callback = require("gitlinker.actions").open_in_browser, print_url = false }
+          )
+        end,
+        desc = "Open git link in browser",
+        mode = { "n" },
+      },
+      {
+        "<leader>gb",
+        function()
+          require("gitlinker").get_buf_range_url(
+            "v",
+            { action_callback = require("gitlinker.actions").open_in_browser, print_url = false }
+          )
+        end,
+        desc = "Open git link in browser",
+        mode = { "v" },
+      },
+      {
+        "<leader>gB",
+        function()
+          require("gitlinker").get_repo_url({
+            action_callback = require("gitlinker.actions").open_in_browser,
+            print_url = false,
+          })
+        end,
+        desc = "Open repo in browser",
+        mode = { "n" },
+      },
+    },
   },
 }
