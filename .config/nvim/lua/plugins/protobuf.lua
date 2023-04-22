5f9af7d2162a91fdcec8b038707c3dd42bcedbbc
Will Green <will@hotgazpacho.org>
2023-04-21 23:48:57 -0400

    feat(nvim): add lsp and linting for protobufs

---
+++ .config/nvim/lua/plugins/protobuf.lua
@@ -0,0 +1,28 @@
+return {
+  -- add proto to treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "proto" })
+      end
+    end,
+  },
+  -- setup lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        bufls = {},
+      },
+    },
+  },
+  -- add buf diagnostics
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sources, require("null-ls.builtins.diagnostics.buf"))
+    end,
+  },
+}
