70a48e6598d0d678cf471e19a8ffa7cf6bd16e1e
Will Green <will@hotgazpacho.org>
2023-04-25 22:52:15 -0400

    fix(nvim): update LuaSnip options to prevent cursor jumps

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -3,6 +3,11 @@ return {
   -- first: disable default <tab> and <s-tab> behavior in LuaSnip
   {
     "L3MON4D3/LuaSnip",
+    opts = {
+      history = true,
+      region_check_events = "InsertEnter",
+      delete_check_events = "TextChanged,InsertLeave",
+    },
     keys = function()
       return {}
     end,
