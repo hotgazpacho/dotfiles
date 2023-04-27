0e92370ef47ac585980a5abfdd938a08b4183e53
Will Green <will@hotgazpacho.org>
2023-04-27 15:37:06 -0400

    feat(nvim): add smartcolumn

---
+++ .config/nvim/lua/plugins/rulers.lua
@@ -0,0 +1,12 @@
+return {
+  "m4xshen/smartcolumn.nvim",
+  opts = {
+    colorcolumn = {
+      "100",
+    },
+    custom_colorcolumn = {
+      proto = "80",
+    },
+    disabled_filetypes = { "NeoTree", "help", "text", "lazy", "mason", "alpha" },
+  },
+}
