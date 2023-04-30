9238b704809d9b52191ac9100869448d23b52755
Will Green <will@hotgazpacho.org>
2023-04-30 09:47:45 -0400

    fix(nvim): ensure that flatten blocks for git commits and markdown for gh pr

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -5,6 +5,11 @@ return {
     lazy = false,
     priority = 1001,
     opts = {
+      -- <String, Bool> dictionary of filetypes that should be blocking
+      block_for = {
+        gitcommit = true,
+        markdown = true, -- gh pr create opens a markdown file to create the pr
+      },
       window = {
         open = "alternate",
       },
