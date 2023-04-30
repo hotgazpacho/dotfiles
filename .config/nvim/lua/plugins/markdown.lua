2e6cd0be242a554ac9202b85b7ef6b182952a2cf
Will Green <will@hotgazpacho.org>
2023-04-30 08:34:25 -0400

    feat(nvim): add markdownlint diagnostics & formatting

---
+++ .config/nvim/lua/plugins/markdown.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "markdownlint", "marksman" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- make sure mason installs the server
+      servers = {
+        marksman = {},
+      },
+    },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sources, require("null-ls.builtins.diagnostics.markdownlint"))
+      table.insert(opts.sources, require("null-ls.builtins.formatting.markdownlint"))
+    end,
+  },
+}
