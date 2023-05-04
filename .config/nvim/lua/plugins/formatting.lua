7f292373a893fa8aa03deb29d564c605b4116e8e
Will Green <will@hotgazpacho.org>
2023-05-04 18:44:30 -0400

    feat(nvim): format json with jq

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -10,6 +10,7 @@ return {
     opts = function(_, opts)
       table.insert(opts.sources, require("null-ls.builtins.formatting.clang_format"))
       table.insert(opts.sources, require("null-ls.builtins.formatting.prettierd"))
+      table.insert(opts.sources, require("null-ls.builtins.formatting.jq"))
     end,
   },
 }
