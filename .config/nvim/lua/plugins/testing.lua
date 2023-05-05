7012b94b093bff76f9abbbe1d78d4619f8604a6a
Will Green <will@hotgazpacho.org>
2023-05-05 17:07:09 -0400

    feat(nvim): use vim-test for testing

---
+++ .config/nvim/lua/plugins/testing.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "vim-test/vim-test",
+    dependencies = {
+      {
+        "folke/which-key.nvim",
+        opts = {
+          defaults = {
+            ["<leader>t"] = { name = "+test" },
+          },
+        },
+      },
+    },
+    config = true,
+    init = function()
+      vim.g["test#strategy"] = "neovim"
+    end,
+    keys = {
+      { "<leader>tt", "<cmd>TestNearest<cr>", desc = "Test nearest" },
+      { "<leader>tf", "<cmd>TestFile<cr>", desc = "Test File" },
+      { "<leader>ts", "<cmd>TestSuite<cr>", desc = "Test Suite" },
+      { "<leader>tl", "<cmd>TestLast<cr>", desc = "Test Last" },
+      { "<leader>tv", "<cmd>TestVisit", desc = "Visit test file" },
+    },
+  },
+}
