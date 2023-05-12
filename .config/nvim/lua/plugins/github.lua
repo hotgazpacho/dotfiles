e13942d7d6723af77c8580072df3aa555120436d
Will Green <will@hotgazpacho.org>
2023-05-12 15:01:04 -0400

    chore(nvim): remove Octo plugin
    Ended up not actually using it.

--- .config/nvim/lua/plugins/github.lua
+++ .config/nvim/lua/plugins/github.lua
@@ -8,45 +8,6 @@ return {
     end,
   },
   {
-    "pwntester/octo.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-      {
-        "folke/which-key.nvim",
-        opts = {
-          defaults = {
-            ["<leader>gb"] = { name = "+github" },
-            ["<leader>gp"] = { name = "+pullrequest" },
-            ["<leader>gr"] = { name = "+review" },
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("octo").setup(opts)
-    end,
-    cmd = "Octo",
-    keys = {
-      { "<leader>gbs", "<cmd>Octo search<cr>", desc = "Search" },
-      { "<leader>gbb", "<cmd>Octo repo browser<cr>", desc = "open in Browser" },
-      -- pullrequest
-      { "<leader>gpc", "<cmd>Octo pr checks<cr>", desc = "Checks status" },
-      { "<leader>gps", "<cmd>Octo pr search<cr>", desc = "Search" },
-      { "<leader>gpr", "<cmd>Octo pr ready<cr>", desc = "mark draft Ready" },
-      { "<leader>gpb", "<cmd>Octo pr browser<cr>", desc = "view in Browser" },
-      { "<leader>gpu", "<cmd>Octo pr url<cr>", desc = "copy URL" },
-      -- review
-      { "<leader>grs", "<cmd>Octo review start<cr>", desc = "Start" },
-      { "<leader>grr", "<cmd>Octo review resume<cr>", desc = "Resume" },
-      { "<leader>grc", "<cmd>Octo review comments<cr>", desc = "Comments" },
-      { "<leader>grC", "<cmd>Octo review commit<cr>", desc = "Commit" },
-      { "<leader>grS", "<cmd>Octo review submit<cr>", desc = "Submit" },
-      { "<leader>grx", "<cmd>Octo review close<cr>", desc = "Close review window" },
-      { "<leader>grD", "<cmd>Octo review discard<cr>", desc = "Discard pending review" },
-    },
-  },
-  {
     "ruifm/gitlinker.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
