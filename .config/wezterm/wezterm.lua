771744a00438e25b1ac02400ffcd14830ede32dd
Will Green <will@hotgazpacho.org>
2024-07-07 10:45:40 -0400

    fix(wezterm): update config to reflect API changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,90 +1,100 @@
 local wezterm = require("wezterm")
 
 local process_icons = {
-	["docker"] = {
-		{ Text = wezterm.nerdfonts.linux_docker },
-	},
-	["docker-compose"] = {
-		{ Text = wezterm.nerdfonts.linux_docker },
-	},
-	["kuberlr"] = {
-		{ Text = wezterm.nerdfonts.linux_docker },
-	},
-	["kubectl"] = {
-		{ Text = wezterm.nerdfonts.linux_docker },
-	},
-	["nvim"] = {
-		{ Text = wezterm.nerdfonts.custom_vim },
-	},
-	["vim"] = {
-		{ Text = wezterm.nerdfonts.dev_vim },
-	},
-	["node"] = {
-		{ Text = wezterm.nerdfonts.mdi_hexagon },
-	},
-	["zsh"] = {
-		{ Text = wezterm.nerdfonts.dev_terminal },
-	},
-	["bash"] = {
-		{ Text = wezterm.nerdfonts.cod_terminal_bash },
-	},
-	["htop"] = {
-		{ Text = wezterm.nerdfonts.mdi_chart_donut_variant },
-	},
-	["cargo"] = {
-		{ Text = wezterm.nerdfonts.dev_rust },
-	},
-	["go"] = {
-		{ Text = wezterm.nerdfonts.mdi_language_go },
-	},
-	["lazydocker"] = {
-		{ Text = wezterm.nerdfonts.linux_docker },
-	},
-	["git"] = {
-		{ Text = wezterm.nerdfonts.dev_git },
-	},
-	["lazygit"] = {
-		{ Text = wezterm.nerdfonts.dev_git },
-	},
-	["lua"] = {
-		{ Text = wezterm.nerdfonts.seti_lua },
-	},
-	["wget"] = {
-		{ Text = wezterm.nerdfonts.mdi_arrow_down_box },
-	},
-	["curl"] = {
-		{ Text = wezterm.nerdfonts.mdi_flattr },
-	},
-	["gh"] = {
-		{ Text = wezterm.nerdfonts.dev_github_badge },
-	},
-	["python"] = {
-		{ Text = wezterm.nerdfonts.dev_python },
-	},
-	["python3"] = {
-		{ Text = wezterm.nerdfonts.dev_python },
-	},
-	["ruby"] = {
-		{ Text = wezterm.nerdfonts.dev_ruby },
-	},
+	["docker"] = wezterm.nerdfonts.linux_docker,
+	["docker-compose"] = wezterm.nerdfonts.linux_docker,
+	["k9s"] = wezterm.nerdfonts.md_kubernetes,
+	["kubectl"] = wezterm.nerdfonts.md_kubernetes,
+	["nvim"] = wezterm.nerdfonts.linux_neovim,
+	["vim"] = wezterm.nerdfonts.dev_vim,
+	["node"] = wezterm.nerdfonts.md_hexagon,
+	["zsh"] = wezterm.nerdfonts.dev_terminal,
+	["bash"] = wezterm.nerdfonts.cod_terminal_bash,
+	["htop"] = wezterm.nerdfonts.md_chart_donut_variant,
+	["cargo"] = wezterm.nerdfonts.dev_rust,
+	["go"] = wezterm.nerdfonts.md_language_go,
+	["lazydocker"] = wezterm.nerdfonts.linux_docker,
+	["git"] = wezterm.nerdfonts.dev_git,
+	["lazygit"] = wezterm.nerdfonts.dev_git,
+	["eza"] = wezterm.nerdfonts.md_file_tree,
+	["fzf"] = wezterm.nerdfonts.md_text_search_variant,
+	["lua"] = wezterm.nerdfonts.seti_lua,
+	["wget"] = wezterm.nerdfonts.md_arrow_down_box,
+	["curl"] = wezterm.nerdfonts.md_flattr,
+	["gh"] = wezterm.nerdfonts.cod_github,
+	["python"] = wezterm.nerdfonts.dev_python,
+	["python3"] = wezterm.nerdfonts.dev_python,
+	["ruby"] = wezterm.nerdfonts.dev_ruby,
 }
 
-local function get_current_working_dir(tab)
-	local current_dir = tab.active_pane.current_working_dir
-	local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
-
-	return current_dir == HOME_DIR and "." or string.gsub(current_dir.string, "(.*[/\\])(.*)", "%2")
+-- Equivalent to POSIX basename(3)
+-- Given "/foo/bar" returns "bar"
+-- Given "c:\\foo\\bar" returns "bar"
+local function basename(s)
+	return string.gsub(s, "(.*[/\\])(.*)", "%2")
 end
 
 local function get_process(tab)
-	local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+	local process_name = basename(tab.active_pane.foreground_process_name)
 	if string.find(process_name, "kubectl") then
 		process_name = "kubectl"
 	end
 
-	return wezterm.format(process_icons[process_name] or { { Text = string.format("[%s]", process_name) } })
+	--local icon = process_icons[process_name] or string.format("[%s]", process_name)
+	local icon = process_icons[process_name] or wezterm.nerdfonts.seti_checkbox_unchecked
+
+	return icon
 end
 
+-- wezterm.gui is not available to the mux server, so take care to
+-- do something reasonable when this config is evaluated by the mux
+local function get_appearance()
+	if wezterm.gui then
+		return wezterm.gui.get_appearance()
+	end
+	return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Catppuccin Macchiato"
+	else
+		return "Catppuccin Latte"
+	end
+end
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+-- Colorscheme
+config.color_scheme = scheme_for_appearance(get_appearance())
+
+-- Font
+config.font = wezterm.font_with_fallback({
+	"Fira Code",
+	{ family = "Symbols Nerd Font Mono", scale = 1.0 },
+})
+config.font_size = 13.0
+
+-- Window
+config.initial_rows = 60
+config.initial_cols = 120
+config.native_macos_fullscreen_mode = true
+config.window_frame = {
+	font_size = 14,
+}
+
+-- Tab/Status bar
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = false
+config.tab_max_width = 60
+config.tab_bar_at_bottom = false
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
 	local has_unseen_output = false
 	if not tab.is_active then
@@ -96,13 +106,7 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 		end
 	end
 
-	local title = string.format(
-		" %s  %s ~ %s  ⌘%d ",
-		wezterm.nerdfonts.fa_chevron_right,
-		get_process(tab),
-		get_current_working_dir(tab),
-		tab.tab_index + 1
-	)
+	local title = string.format(" %s %s  ⌘%d ", get_process(tab), tab.active_pane.title, tab.tab_index + 1)
 
 	if has_unseen_output then
 		return {
@@ -116,39 +120,14 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 	}
 end)
 
--- wezterm.gui is not available to the mux server, so take care to
--- do something reasonable when this config is evaluated by the mux
-function get_appearance()
-	if wezterm.gui then
-		return wezterm.gui.get_appearance()
-	end
-	return "Dark"
-end
+wezterm.on("update-right-status", function(window, pane)
+	local date = wezterm.strftime("%a %b %-d %-I:%M %p")
 
-function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Macchiato"
-	else
-		return "Catppuccin Latte"
-	end
-end
+	-- Make it italic and underlined
+	window:set_right_status(wezterm.format({
+		{ Attribute = { Italic = true } },
+		{ Text = " " .. wezterm.nerdfonts.md_calendar_clock_outline .. " " .. date .. " " },
+	}))
+end)
 
-return {
-	font = wezterm.font_with_fallback({
-		"Fira Code",
-		{ family = "Symbols Nerd Font Mono", scale = 1.0 },
-	}),
-	font_size = 13.0,
-	use_cap_height_to_scale_fallback_fonts = true,
-	window_frame = {
-		font = wezterm.font_with_fallback({
-			"Fira Code",
-			{ family = "Symbols Nerd Font Mono", scale = 1.0 },
-		}),
-	},
-	color_scheme = scheme_for_appearance(get_appearance()),
-	hide_tab_bar_if_only_one_tab = true,
-	initial_rows = 60,
-	initial_cols = 120,
-	native_macos_fullscreen_mode = true,
-}
+return config
