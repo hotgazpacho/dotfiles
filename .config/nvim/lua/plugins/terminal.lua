5244e4a4af7269b4c8aabfc5747bd9f29a6c50c5
Will Green <will@hotgazpacho.org>
2024-07-07 13:06:47 -0400

    feat(nvim): enable edgy

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -9,10 +9,12 @@ return {
       -- <String, Bool> dictionary of filetypes that should be blocking
       block_for = {
         gitcommit = true,
+        gitrebase = true,
         markdown = true, -- gh pr create opens a markdown file to create the pr
       },
       window = {
         open = "alternate",
+        diff = "tab_vsplit",
         focus = "first",
       },
     },
