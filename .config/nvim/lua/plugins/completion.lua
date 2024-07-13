534756a081d0917fc4ff43298d346a0fc946b241
Will Green <will@hotgazpacho.org>
2024-07-13 19:03:03 -0400

    feat(nvim): dump LuaSnip and the custom completion shortcuts

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,6 +1,4 @@
 return {
-  -- Supertab
-  -- Use <tab> for completion and snippets (supertab).
   {
     "hrsh7th/nvim-cmp",
     dependencies = {
@@ -8,43 +6,11 @@ return {
     },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
-      table.insert(opts.sources, { name = "emoji" })
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
       local cmp = require("cmp")
+      local types = require("cmp.types")
 
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["S-Esc"] = cmp.mapping.abort(),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
-            cmp.config({ select = true })
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(1)
-            end)
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = -1 }) then
-            vim.schedule(function()
-              vim.snippet.jump(-1)
-            end)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
+      -- Add emoji completion
+      table.insert(opts.sources, { name = "emoji" })
     end,
   },
 }
