3d8e352452e3cabade77e4b175258e44862ef921
Will Green <will@hotgazpacho.org>
2024-08-19 22:31:33 -0400

    feat(nvim): add precognition.nvim
    https://github.com/tris203/precognition.nvim

---
+++ .config/nvim/lua/plugins/motions.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "tris203/precognition.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return {
+        startVisible = true,
+      }
+    end,
+    keys = {
+      {
+        "<leader>uP",
+        function()
+          if require("precognition").toggle() then
+            LazyVim.info("precognition on")
+          else
+            LazyVim.info("precognition off")
+          end
+        end,
+        desc = "Toggle precognition",
+      },
+      {
+        "<leader>uk",
+        function()
+          require("precognition").peek()
+        end,
+        desc = "Show precognition until next motion",
+      },
+    },
+  },
+}
