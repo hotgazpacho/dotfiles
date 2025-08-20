4c5b63c27fdf70ed16933a7a4ad088deeb298ec9
Will Green <will@hotgazpacho.org>
2025-08-20 08:35:03 -0400

    fix(nvim): workaround for catppuccin's breaking change
    See https://github.com/LazyVim/LazyVim/pull/6354#issuecomment-3203793593

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "catppuccin",
+    optional = true,
+    opts = function()
+      local bufferline = require("catppuccin.groups.integrations.bufferline")
+      bufferline.get = bufferline.get or bufferline.get_theme
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-macchiato",
