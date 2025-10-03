b86a97fe907054d5756f8972ff9342485f037aee
Will Green <will@hotgazpacho.org>
2025-10-03 08:42:31 -0400

    chore(nvim): update plugins and config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,6 @@ return {
   {
     "catppuccin",
     optional = true,
-    opts = function()
-      local bufferline = require("catppuccin.groups.integrations.bufferline")
-      bufferline.get = bufferline.get or bufferline.get_theme
-    end,
   },
   {
     "LazyVim/LazyVim",
