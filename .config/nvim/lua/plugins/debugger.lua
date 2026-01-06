b289a677cfcdbc4579bcc3d5f656d3df7774e8b3
Will Green <will@hotgazpacho.org>
2026-01-06 07:55:13 -0500

    fix(nvim): don't try to load lua 5.1 & jsonc

--- .config/nvim/lua/plugins/debugger.lua
+++ .config/nvim/lua/plugins/debugger.lua
@@ -1,17 +1,17 @@
 return {
-  {
-    "Joakker/lua-json5",
-    build = "./install.sh",
-    lazy = false,
-    priority = 1000,
-    enabled = not vim.g.vscode,
-  },
+  --  {
+  --    "Joakker/lua-json5",
+  --    build = "./install.sh",
+  --    lazy = false,
+  --    priority = 1000,
+  --    enabled = not vim.g.vscode,
+  --  },
   {
     "mfussenegger/nvim-dap",
     enabled = not vim.g.vscode,
-    config = function()
-      require("dap.ext.vscode").json_decode = require("json5").parse
-    end,
+    --config = function()
+    --  require("dap.ext.vscode").json_decode = require("json5").parse
+    -- end,
     keys = {
       { "<leader>dt", false },
       {
